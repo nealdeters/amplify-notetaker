dee3e6a7cc271d27c0aba0e9f64dfe693c8f2ea5
Neal Deters <nealdeters@gmail.com>
2020-07-24 23:48:51 -0500

    feat: created api and started work on ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { withAuthenticator } from 'aws-amplify-react'
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([
+    {id: 1, note: 'Hello world'}
+  ]);
+  const [note, setNote] = useState("");
+  const [noteId, setNoteId] = useState("");
+  const [noteIndex, setNoteIndex] = useState("");
+  const [deletingId, setDeletingId] = useState("");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-1">Amplify Notetaker</h1>
+      <form className="mb3">
+        <input className="pa2 f4" type="text" placeholder="Write your note" />
+        <button className="pa2 f4" type="submit">Add Note</button>
+      </form>
+
+      <div>
+        {notes.map(item => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">
+              {item.note}
+            </li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
